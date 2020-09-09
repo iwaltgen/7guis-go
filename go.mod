fdbd64dd8315a80fb8e459455eb540389f15d2ea
iwaltgen <iwaltgen@gmail.com>
2020-09-09 12:47:35 +0900

    Add dependency file and build a script file.

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/iwaltgen/7guis-gio-fyne
+
+go 1.15
+
+require (
+	fyne.io/fyne v1.3.3
+	gioui.org v0.0.0-20200829162755-829ee4559c5a
+	github.com/magefile/mage v1.10.0
+)
---
+++ magefile.go
@@ -0,0 +1,54 @@
+// +build mage
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+	"github.com/magefile/mage/target"
+)
+
+func Clean() {
+	os.RemoveAll("build")
+}
+
+func Build() error {
+	sources := []string{
+		"./fyne/counter",
+		"./fyne/temperature-converter",
+		"./gio/counter",
+		"./gio/temperature-converter",
+	}
+
+	goexe := mg.GoCmd()
+	for _, source := range sources {
+		if err := build(goexe, source); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func build(goexe, source string) error {
+	dir := filepath.Join("build", filepath.Dir(source))
+	file := filepath.Join("build", source)
+	if midified, err := target.Dir(file, source); err != nil {
+		return fmt.Errorf("check target modified error: %w", err)
+	} else if !midified {
+		return nil
+	}
+
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		return fmt.Errorf("mkdir error: %w", err)
+	}
+
+	return sh.RunV(goexe, "build", "-trimpath", "-ldflags", "-s -w", "-o", file, source)
+}
+
+func UPX() {
+	_ = sh.RunV("sh", "-c", "upx build/**/*")
+}
